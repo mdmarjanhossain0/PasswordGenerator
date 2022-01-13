ab2ddf0084e1d221e5cb8e79777ec0792e9dc9f2
mdmarjanhossain0 <mdmarjanhossain0@gmail.com>
2022-01-13 19:43:39 +0600

    save image

--- generator/views.py
+++ generator/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
 
 def upload_image_view(request):
-    image = request.FILES['image']
-    print(image)
+
+    if request.method == "POST":
+        file = request.FILES['image']
+        print(file)
+       
+        file_name = default_storage.save(file.name, file)
+        file = default_storage.open(file_name)
+        file_url = default_storage.url(file_name)
+        print(file_url)
 
     
     return render(request, 'upload.html', {})
+
+    data['name'] = password
